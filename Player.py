4287d3de6bcb92180f54555d3dab5c4b41984de4
Kostia-Hn <gnennyy@gmail.com>
2020-04-19 16:59:57 +0300

    Update Player.py

--- Player.py
+++ Player.py
@@ -14,3 +14,15 @@ class Player:
          for card in self.hand:
             card.show()
 
+player_1 = Player("Kostia")
+player_2 = Player("Ilia")
+# Этап 1, раздача карт, определение козыря
+deck_1 = Deck()
+deck_1.shuffle()
+
+player_1.draw_card(deck_1)
+player_1.draw_card(deck_1)
+player_1.show_hand()
+
+player_2.draw_card(deck_1)
+player_2.show_hand()
